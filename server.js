7b0a0df6b081a88d2995c3d2599c0a8c76c199f6
Glitch (plastic-rib) <none>
2017-11-11 08:04:10 +0000

    updated

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
 var app = express();
+var Yelp = require('node-yelp-fusion');
+var yelp=new Yelp({ id:"rGdNsmucbSDO38xCi10UiQ" , secret:"F2eU5bGtTjQm6BhCbaf9zHyGZpmGcikh2H6fUtSnwWp68dYphIaXUTi7YAZafF6I" });
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.get("/", function(req,res){
+	res.sendFile(__dirname+"/views/index.html")
+})
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+
+app.post("/searching", function(req,res){
+	   
+	var search = {
+	 location: req.body.city
+	}
+	//sender is the array to go to cookies for display for location names via locs cookie
+	var sender = []
+	var imgArr = []
+	yelp.search("categories=nightlife&location="+search.location)
+    .then(function(result){
+   		result["businesses"].forEach((x)=>{
+    		sender.push(x.name)
+    		imgArr.push(x.image_url)
+    	})    	   	  
+    	  		res.cookie("locs",sender);
+    	  		res.cookie("photos",imgArr);
+    		res.redirect("/")
+        });
+
+})
+app.listen(3000,function(req,res){
+	console.log("Listening on port 3000...")
+})
